3243625d8ee749dadc5ccd81603fea79cf54d636
hougl <hgl868@126.com>
2019-11-05 15:46:13 +0800

    add testParameter

--- crack/src/main/cpp/hellojni.c
+++ crack/src/main/cpp/hellojni.c
@@ -93,6 +93,25 @@ void testProperties()
     return;
 }
 
+void testParameter(int a1,  int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10,
+          int a11,  int a12, int a13, int a14, int a15, int a16, int a17, int a18, int a19, int a20,
+          int a21, int a22, int a23, int a24, int a25, int a26, int a27, int a28, int a29, int a30,
+          int a31, int a32, int a33, int a34, int a35, int a36, int a37, int a38, int a39, int a40,
+          int a41, int a42, int a43, int a44, int a45, int a46, int a47, int a48, int a49, int a50)
+{
+    LOGE("%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, \
+                 %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, \
+                 %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, \
+                 %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, \
+                 %d, %d, %d, %d, %d, %d, %d, %d, %d, %d",
+         a1,   a2, a3,  a4,  a5,  a6,  a7,  a8,  a9,  a10,
+         a11,  a12, a13,  a14,  a15,  a16,  a17,  a18,  a19,  a20,
+         a21,  a22, a23,  a24,  a25,  a26,  a27,  a28,  a29,  a30,
+         a31,  a32, a33,  a34,  a35,  a36,  a37,  a38,  a39,  a40,
+         a41,  a42, a43,  a44,  a45,  a46,  a47,  a48,  a49,  a50);
+    return;
+}
+
 JNIEXPORT void JNICALL
 Java_com_droider_HelloJni_nativeMsg(JNIEnv* env, jobject thiz)
 {
@@ -106,6 +125,12 @@ Java_com_droider_HelloJni_nativeMsg(JNIEnv* env, jobject thiz)
 
     pid_t pid = getpid();
     uid_t uid = getuid();
+
+    testParameter(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                  11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                  21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+                  31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+                  41, 42, 43, 44, 45, 46, 47, 48, 49 ,50);
 
     char *username = getlogin();
     LOGE("F:%s,%s", __FUNCTION__, username);
